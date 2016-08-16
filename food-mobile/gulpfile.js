f3754afc78bd7b629ee56461a495fdff755fc80e
Johannes Petrus Huisamen <jphuisamen@gmail.com>
2016-08-16 12:39:01 +0200

    initial deveopment server setup

--- food-mobile/gulpfile.js
+++ food-mobile/gulpfile.js
@@ -1,6 +1,9 @@
 const gulp = require('gulp');
+const server = require('gulp-server-livereload');
 
 gulp.task('default', function(cb){
+  return gulp.src('www')
+    .pipe(server({livereload: true}))
   console.log('default task')
   cb()
 })
